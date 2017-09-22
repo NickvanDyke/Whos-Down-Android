93af32abbebef625b32aecde7fd160bd0689bd2f
NickvanDyke <nickjvandyke15@gmail.com>
2017-09-22 02:47:24 -0500

    actual logo courtesy of Jeff, properly clear focus from edittext

--- app/src/main/java/com/vandyke/whosdown/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/vandyke/whosdown/ui/main/view/MainActivity.kt
@@ -9,15 +9,14 @@ import android.content.res.Resources
 import android.databinding.DataBindingUtil
 import android.databinding.Observable
 import android.databinding.ObservableBoolean
-import android.graphics.Rect
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
-import android.view.MotionEvent
 import android.view.View
+import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -69,32 +68,42 @@ class MainActivity : Activity(), PopupMenu.OnMenuItemClickListener {
                     // TODO: maybe fade the logo or something. Should wait until I have an actual logo to see what looks best
                     val resizeAnimation = ResizeAnimation(downLayout, pixelHeight, downWrapContentHeight)
                     resizeAnimation.duration = 300
-                    resizeAnimation.setAnimationListener(object : Animation.AnimationListener {
-                        override fun onAnimationEnd(animation: Animation?) {}
+
+                    val fadeAnimation = AlphaAnimation(1.0f, 0.0f)
+                    fadeAnimation.duration = 300
+                    fadeAnimation.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationEnd(animation: Animation?) {
+                            downLogo.visibility = View.INVISIBLE
+                        }
+
                         override fun onAnimationRepeat(animation: Animation?) {}
                         override fun onAnimationStart(animation: Animation?) {
-                            downLogo.visibility = View.INVISIBLE
                         }
                     })
 
+                    downLogo.startAnimation(fadeAnimation)
                     downLayout.startAnimation(resizeAnimation)
                 } else {
                     val resizeAnimation = ResizeAnimation(downLayout, downWrapContentHeight, pixelHeight)
                     resizeAnimation.duration = 300
-                    resizeAnimation.setAnimationListener(object : Animation.AnimationListener {
-                        override fun onAnimationEnd(animation: Animation?) {
+
+                    val fadeAnimation = AlphaAnimation(0.0f, 1.0f)
+                    fadeAnimation.duration = 300
+                    fadeAnimation.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationEnd(animation: Animation?) {}
+                        override fun onAnimationRepeat(animation: Animation?) {}
+                        override fun onAnimationStart(animation: Animation?) {
                             downLogo.visibility = View.VISIBLE
                         }
-                        override fun onAnimationRepeat(animation: Animation?) {}
-                        override fun onAnimationStart(animation: Animation?) {}
                     })
 
+                    downLogo.startAnimation(fadeAnimation)
                     downLayout.startAnimation(resizeAnimation)
                 }
             }
         })
 
-        /* set RecyclerView stuff */
+        /* set peeps list stuff */
         peepsList.addItemDecoration(DividerItemDecoration(peepsList.context, (peepsList.layoutManager as LinearLayoutManager).orientation))
         peepsList.adapter = PeepsAdapter(viewModel)
 
@@ -105,17 +114,24 @@ class MainActivity : Activity(), PopupMenu.OnMenuItemClickListener {
             peepsListSwipe.isRefreshing = false
         }
 
-        /* update the viewModel's message when the EditText loses focus (which is caused when somewhere else is touched, or done is pressed on kb) */
+        /* update the viewModel's message and hide the keyboard when the EditText loses focus */
         userMessage.onFocusChangeListener = View.OnFocusChangeListener { view, b ->
             viewModel.message.set((view as EditText).text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
 
         /* close the keyboard and clear focus from message box when done is pressed on the keyboard */
         userMessage.setOnEditorActionListener { textView, i, keyEvent ->
             userMessage.clearFocus()
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(textView.windowToken, 0)
             true
+        }
+
+        /* since the downSwitch isn't focusable, need to manually clear focus from the EditText when it's touched */
+        downSwitch.setOnTouchListener { view, motionEvent ->
+            if (currentFocus is EditText)
+                currentFocus.clearFocus()
+            false
         }
     }
 
@@ -137,22 +153,5 @@ class MainActivity : Activity(), PopupMenu.OnMenuItemClickListener {
         popupMenu.inflate(R.menu.main_popup)
         popupMenu.setOnMenuItemClickListener(this)
         popupMenu.show()
-    }
-
-    /* clears EditText focus and hides keyboard when tapping anywhere else */
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is EditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.windowToken, 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(event)
     }
 }
